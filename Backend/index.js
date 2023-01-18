5503a29344eba347997a8ccd8fb5b6ef250ff25c
setu bhaskar jha <sbhaskar1998@gmail.com>
2023-01-18 20:27:38 +0530

    added: tv schema and model and product controller

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
+const productsRouter = require("./Routes/products.route");
 
 const { connection } = require("./Config/db");
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/products", productsRouter);
 
 app.listen(process.env.port, async () => {
   try {
