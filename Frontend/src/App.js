b06288508e0835f8bf58d42dc39cbe8d10f90e15
Suraj Jadhav <jadhavsuraj4567@gmail.com>
2023-01-19 20:32:00 +0530

    Adding complete login and signup fromtend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,18 @@
+// function App() {
+//   return <div className="App">React App</div>;
+// }
+
+import Login from "./Authentication/login";
+import Signup from "./Authentication/signup";
+
+// export default App;
 function App() {
-  return <div className="App">React App</div>;
+  return (
+    <div className="App">
+      <Login />
+      {/* <Signup /> */}
+    </div>
+  );
 }
 
 export default App;
