e72a2303b177cb3e020b18463d5343a0a1778d89
18Kailash <sumer.ki359@gmail.com>
2023-01-19 22:08:39 +0530

    day-3 task

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,12 @@
 
+
 function App() {
   return (
     <div className="App">
 
-  React App
-
-  React App
+   React App
+  
+    
     </div>
   );
 }
--- Frontend/src/productcomponents/components/Filter.jsx
+++ Frontend/src/productcomponents/components/Filter.jsx
@@ -2,7 +2,510 @@ import React from 'react'
 
 const Filter = () => {
   return (
-    <div>
+    <div style={{border:"1px solid red" ,width:"20%",fontSize:"16px"}}>
+      <div>
+      <h1>FILTER COMPONENT</h1>
+      </div>
+
+             <div>
+          <h1>Price</h1>
+          <div >
+          <input 
+          type="checkbox"/>
+         <label>Rs. 7000 - Rs. 9999 (10)</label>
+        
+          </div>
+          <div>
+          <input 
+          type="checkbox"
+         
+        
+         />
+         <label>Rs. 10000 and Above (17)</label>
+        
+          </div>
+             </div>
+              <br/>
+              <hr/>
+         
+      
+
+
+           <div>
+          <h1>Brand</h1>
+           <div>
+          <input 
+          type="checkbox"
+         
+        
+         />
+         <label>Asus (1)</label>
+        
+           </div>
+
+           <div>
+          <input 
+          type="checkbox"
+         
+        
+         />
+         <label>Asus</label>
+        
+           </div>
+
+           <div>
+          <input 
+          type="checkbox"
+         
+        
+         />
+         <label>Blackberry (4)</label>
+        
+           </div>
+
+           <div>
+          <input 
+          type="checkbox"
+         
+        
+         />
+         <label>Huawei (1) (4)</label>
+        
+           </div>
+
+           <div>
+          <input 
+          type="checkbox"
+         
+        
+         />
+         <label>I KALL (7) (4)</label>
+        
+           </div>
+
+
+           <div>
+          <input 
+          type="checkbox"
+         
+        
+         />
+         <label>Micromax (4)</label>
+        
+           </div>
+
+
+
+
+
+           </div>
+           <br/>
+            <hr/>
+
+
+
+            <div>
+              <h1>Discount</h1>
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label>       20%</label>
+            
+              </div>
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label>          30%</label>
+            
+              </div>
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label>         40%</label>
+            
+              </div>
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label>         50%</label>
+            
+              </div>
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label>         60%</label>
+            
+              </div>
+
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label>{"     "}70%</label>
+            
+              </div>
+
+
+
+
+
+            </div>
+            <br/>
+            <hr/>
+
+
+
+            <div>
+              <h1>Primary Camera</h1>
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 12MP</label>
+            
+              </div>
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 13MP</label>
+            
+              </div>
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 14MP</label>
+            
+              </div>
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 15MP</label>
+            
+              </div>
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 16MP</label>
+            
+              </div>
+
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 17MP</label>
+            
+              </div>
+
+
+
+
+
+            </div>
+            <br/>
+            <hr/>
+
+
+
+            <div>
+              <h1>Front Camera</h1>
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 1MP</label>
+            
+              </div>
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 2MP</label>
+            
+              </div>
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 3MP</label>
+            
+              </div>
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 4MP</label>
+            
+              </div>
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 5MP</label>
+            
+              </div>
+
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 6MP</label>
+            
+              </div>
+
+
+
+
+
+            </div>
+            <br/>
+            <hr/>
+
+
+            <div>
+              <h1>RAM</h1>
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 2GB</label>
+            
+              </div>
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label>3GB</label>
+            
+              </div>
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 4GB</label>
+            
+              </div>
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 5GB</label>
+            
+              </div>
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 6GB</label>
+            
+              </div>
+
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 7GB</label>
+            
+              </div>
+
+
+
+
+
+            </div>
+            <br/>
+            <hr/>
+
+
+
+
+             <div>
+              <h1>Internal Memory</h1>
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 256GB</label>
+            
+              </div>
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label>128GB</label>
+            
+              </div>
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 64GB</label>
+            
+              </div>
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 32GB</label>
+            
+              </div>
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 16GB</label>
+            
+              </div>
+
+
+              
+
+
+
+
+
+             </div>
+             <br/>
+             <hr/>
+
+
+             <div>
+              <h1>Product Rating</h1>
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 3 & above (1)</label>
+            
+              </div>
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 2 & above (1)</label>
+            
+              </div>
+
+              <div>
+              <input 
+              type="checkbox"
+            
+            
+            />
+            <label> 1& above (1)</label>
+            
+              </div>
+             </div>
+             <br/>
+             <hr/>
+
+             
+
+              
+
+
+
+
+
+
+
+
       
     </div>
   )
