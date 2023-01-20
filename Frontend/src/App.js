5173350a9de620c46bab2e3fd464a52848a18dca
Suraj Jadhav <jadhavsuraj4567@gmail.com>
2023-01-20 17:41:06 +0530

    minor changes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,16 +1,5 @@
-// function App() {
-//   return <div className="App">React App</div>;
-// }
-
-const { default: AdminPage } = require("./Authentication/AdminPage");
-
-// export default App;
-
 function App() {
-  return (
-    <div>
-      <AdminPage />
-    </div>
-  );
+  return <div className="App">React App</div>;
 }
+
 export default App;
