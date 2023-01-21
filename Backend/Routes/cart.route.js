123d7e7c5775158be77d5237005772f892a2c104
setu bhaskar jha <sbhaskar1998@gmail.com>
2023-01-21 10:46:44 +0530

    added: lost codes

--- Backend/Routes/cart.route.js
+++ Backend/Routes/cart.route.js
@@ -4,7 +4,53 @@ const Cart = require("../Models/cart.model");
 
 const cartRouter = express.Router();
 
-cartRouter.get("", Authentication, (req, res) => {});
+cartRouter.get("", Authentication, async (req, res) => {
+  try {
+    const cart = await Cart.find({ userId: req.body.userId }).lean().exec();
+    return res.status(200).send(cart);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+cartRouter.post("", Authentication, async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const cart = await Cart.create({
+      product_name: req.body.product_name,
+      image: req.body.image,
+      price: req.body.price,
+      shipping: req.body.shipping,
+      userId: userId,
+    });
+    return res.status(201).send(cart);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+cartRouter.patch("/:id", Authentication, async (req, res) => {
+  const payload = req.body;
+  try {
+    const item = await Cart.findByIdAndUpdate(req.params.id, payload, {
+      new: true,
+    })
+      .lean()
+      .exec();
+    return res.status(201).send(item);
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
+});
+
+cartRouter.delete("/:id", Authentication, async (req, res) => {
+  try {
+    const cart = await Cart.findByIdAndDelete(req.params.id).lean().exec();
+    return res.status(200).send("Data succesfully deleted!");
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
+});
 
 module.exports = cartRouter;
 
